4e1de8e84355bcee539a5fbf781c9dbecc453452
smlnkls1208 <al89994950059@gmail.com>
2025-11-05 13:38:25 +0700

    исправления

--- designpro/settings.py
+++ designpro/settings.py
@@ -91,6 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
